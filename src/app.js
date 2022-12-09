5065ae7370a8b25efee853e15bfe596fb6c1019e
eaguilard <eaguilar@uninorte.edu.co>
2022-12-09 12:17:14 -0500

    boton cancelar funcionado

--- src/app.js
+++ src/app.js
@@ -30,9 +30,9 @@ app.get('/', (req, res) =>{
 
 
 // ************ Set the server to listen ************
-app.listen(process.env.PORT, () => {
-    console.log('Server running on port', process.env.PORT);
-});
+app.listen(process.env.PORT || 5000, () => {
+    console.log('Server running on port', process.env.PORT || 5000);
 
+});
 
 export default app;
